57a7c4ae1b253e179b2237db4399330119c70c6f
BlazeWYSB <68539222+BlazeWYSB@users.noreply.github.com>
2020-07-22 00:14:50 +0800

    版本更替
    4.2f1会崩溃，先用稍微旧一点的版本了，做的东西不难，应该不会有兼容性问题

---
+++ TetrisVR_3D/Assets/Resource/script/type_Transform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class type_Transform : MonoBehaviour {
+    public GameObject partA;
+    public GameObject partB;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        { 
+            partA.transform.RotateAround(partB.transform.position+new Vector3(10,0,0), Vector3.up, 90);
+        }
+    }
+}
