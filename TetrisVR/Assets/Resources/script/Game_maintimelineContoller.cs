342768ac540dbfd5ab72dce9d4f62290335bf825
BlazeWYSB <68539222+BlazeWYSB@users.noreply.github.com>
2020-07-23 21:21:40 +0800

    7/23
    test

---
+++ TetrisVR/Assets/Resources/script/Game_maintimelineContoller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_maintimelineContoller : MonoBehaviour
+{
+    public GameObject ActiveObject;
+    public GameObject NextObject;
+    GameObject L_block_perfab;
+    GameObject T_block_perfab;
+    GameObject I_block_perfab;
+    GameObject Trans_block_perfab;
+    Vector3 start_position;
+    Quaternion start_rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_position = new Vector3(0, 11, 0);
+        start_rotation = new Quaternion(90, 0, 0, 0);
+        L_block_perfab = Resources.Load("Prebs/L") as GameObject;
+        T_block_perfab = Resources.Load("Prebs/T") as GameObject;
+        I_block_perfab = Resources.Load("Prebs/I") as GameObject;
+        Trans_block_perfab = (GameObject)Resources.Load("Prebs/Transform");
+        ActiveObject = Instantiate(Trans_block_perfab, start_position, start_rotation);
+        //Debug.Log("???");
+        InvokeRepeating("Block_Drop", 1f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Block_Drop()
+    {
+        ActiveObject.transform.Translate(Vector3.up);
+    }
+}
